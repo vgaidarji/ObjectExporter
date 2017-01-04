b23b0e88b9e934a1d1b5cb6c3c47ba4f1b6b8517
vgaidarji <veaceslav.gaidarji@gmail.com>
2017-01-04 22:48:40 +0200

    Add sample Java project with few objects for debugging

---
+++ sample-java/src/java/SampleJava.java
@@ -0,0 +1,39 @@
+package java;
+
+public class SampleJava {
+
+    static void main(String[] args) {
+        int x = 2;
+        x = 3;
+
+        Person p = new Person("Veaceslav", "Gaidarji",
+                new Address("Chisinau", "bd. Stefan cel Mare", 001), 17);
+        p.age = 18;
+    }
+
+    static class Person {
+        String firstName;
+        String lastName;
+        Address address;
+        int age;
+
+        Person(String firstName, String lastName, Address address, int age) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.age = age;
+        }
+    }
+
+    static class Address {
+        String city;
+        String street;
+        Integer building;
+
+        Address(String city, String street, Integer building) {
+            this.city = city;
+            this.street = street;
+            this.building = building;
+        }
+    }
+}
